42a08d330b930ce1022e98a525cef22795d303aa
Yuda Adi Pratama <yudaa0110@gmail.com>
2022-05-18 11:54:47 +0700

    first commit capstone api

---
+++ capstone-api/server.js
@@ -0,0 +1,11 @@
+/* eslint-disable linebreak-style */
+const Hapi = require('@hapi/hapi');
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+  });
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+init();
