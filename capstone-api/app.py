379a9cfa52229bf299a60c40fed799d8f931b7f4
Yuda Adi Pratama <yudaa0110@gmail.com>
2022-06-13 14:59:26 +0700

    adding new app.py, prediction.py, and new ml file

--- capstone-api/app.py
+++ capstone-api/app.py
@@ -1,98 +1,16 @@
-# Import Module
-import re
 from flask import Flask, request, jsonify
-from tensorflow.keras.models import load_model
-import tensorflow as tf
-import pandas as pd
-import pickle
-import io
-import re
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-
+from prediction import predict
 
 # Initialize Flask
 app = Flask(__name__)
 
-# # Import model
-# model = load_model("chatbot_model.h5")
-# tokenizer = pickle.load(open("tokenizer.pickle", "rb"))
+# Initialize Flask server 
+@app.route("/", methods=["POST"])
+def hello(input, results):
+    input = request.json['message']
 
-# # Maximum sentence length
-# MAX_LENGTH = 40
-
-# # Define start and end token to indicate the start and end of a sentence
-# START_TOKEN, END_TOKEN = [tokenizer.vocab_size], [tokenizer.vocab_size + 1]
-
-# Initialize Flask server with error handling
-@app.route("/")
-def hello():
-    return "Welcome to machine learning model APIs!"
-
-@app.route("/predict", methods=['POST'])
-def preprocess_sentence(sentence):
-  sentence = sentence.lower().strip()
-  # creating a space between a word and the punctuation following it
-  # eg: "he is a boy." => "he is a boy ."
-  sentence = re.sub(r"([?.!,])", r" \1 ", sentence)
-  sentence = re.sub(r'[" "]+', " ", sentence)
-  # removing contractions
-  sentence = re.sub(r"i'm", "i am", sentence)
-  sentence = re.sub(r"he's", "he is", sentence)
-  sentence = re.sub(r"she's", "she is", sentence)
-  sentence = re.sub(r"it's", "it is", sentence)
-  sentence = re.sub(r"that's", "that is", sentence)
-  sentence = re.sub(r"what's", "that is", sentence)
-  sentence = re.sub(r"where's", "where is", sentence)
-  sentence = re.sub(r"how's", "how is", sentence)
-  sentence = re.sub(r"\'ll", " will", sentence)
-  sentence = re.sub(r"\'ve", " have", sentence)
-  sentence = re.sub(r"\'re", " are", sentence)
-  sentence = re.sub(r"\'d", " would", sentence)
-  sentence = re.sub(r"\'re", " are", sentence)
-  sentence = re.sub(r"won't", "will not", sentence)
-  sentence = re.sub(r"can't", "cannot", sentence)
-  sentence = re.sub(r"n't", " not", sentence)
-  sentence = re.sub(r"n'", "ng", sentence)
-  sentence = re.sub(r"'bout", "about", sentence)
-  # replacing everything with space except (a-z, A-Z, ".", "?", "!", ",")
-  sentence = re.sub(r"[^a-zA-Z?.!,]+", " ", sentence)
-  sentence = sentence.strip()
-  return sentence
-
-def evaluate(sentence):
-  sentence = preprocess_sentence(sentence)
-
-  sentence = tf.expand_dims(
-      START_TOKEN + tokenizer.encode(sentence) + END_TOKEN, axis=0)
-
-  output = tf.expand_dims(START_TOKEN, 0)
-
-  for i in range(MAX_LENGTH):
-    predictions = model(inputs=[sentence, output], training=False)
-
-    # select the last word from the seq_len dimension
-    predictions = predictions[:, -1:, :]
-    predicted_id = tf.cast(tf.argmax(predictions, axis=-1), tf.int32)
-
-    # return the result if the predicted_id is equal to the end token
-    if tf.equal(predicted_id, END_TOKEN[0]):
-      break
-
-    # concatenated the predicted_id to the output which is given to the decoder
-    # as its input.
-    output = tf.concat([output, predicted_id], axis=-1)
-
-  return tf.squeeze(output, axis=0)
-
-def predict(sentence):
-  prediction = evaluate(sentence)
-
-  predicted_sentence = tokenizer.decode(
-      [i for i in prediction if i < tokenizer.vocab_size])
- 
-  print('Input: {}'.format(sentence)) ## input dari MD
-  print('Output: {}'.format(predicted_sentence)) ## output dari model ML
+    predicted_message = predict(input)
+    return predicted_message
 
 if __name__ == '__main__':
     # app.run(host='0.0.0.0', port=8080, debug=True)
---
+++ capstone-api/prediction.py
@@ -0,0 +1,42 @@
+import os
+#import shutil
+import pandas as pd
+
+import tensorflow as tf
+import tensorflow_hub as hub
+import tensorflow_text as text
+from sklearn.preprocessing import LabelBinarizer
+
+
+model = tf.keras.models.load_model(('https://storage.googleapis.com/ml-model-chabot/bert-model.h5'), custom_objects={'KerasLayer':hub.KerasLayer})
+
+def predict(inputs, results):
+  result_for_printing = \
+    [f'input: {inputs[i]:<30} : estimated intent: {results[i]}'
+                         for i in range(len(inputs))]
+  print(*result_for_printing, sep='\n')
+  print()
+
+
+examples = [
+    ' i want to fly from boston at 838 am and arrive in denver at 1110 in the morning',  # this is the same sentence tried earlier
+    ' what airlines have flights from baltimore to seattle',
+    ' show me ground transportation in denver',
+    ' what does fare code qo mean'
+]
+
+trainfile="atis_intents_train.csv"
+traindf = pd.read_csv(trainfile)
+traindf.columns = ['intent', 'text']
+traindf = traindf.reindex(columns=['text', 'intent'])
+
+trainfeatures=traindf.copy()
+trainlabels=trainfeatures.pop("intent")
+
+binarizer=LabelBinarizer()
+trainlabels=binarizer.fit_transform(trainlabels.values)
+
+results = tf.nn.softmax(model(tf.constant(examples)))
+intents=binarizer.inverse_transform(results.numpy())
+
+predict(examples, intents)
