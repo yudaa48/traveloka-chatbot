47ce21adc656429f5251fb426da6e62851428cd3
Yuda Adi Pratama <yudaa0110@gmail.com>
2022-06-15 13:27:46 +0700

    adding predictionv3

--- capstone-api/app.py
+++ capstone-api/app.py
@@ -1,17 +1,25 @@
 from flask import Flask, request, jsonify
-from prediction import predicts
+from predictionv3 import get_answer
 
 # Initialize Flask
 app = Flask(__name__)
 
-# Initialize Flask server 
+# Initialize Flask server (file predictionv3.py)
 @app.route("/", methods=["POST"])
 def hello():
     input = request.json['message']
 
-    predicted_message = predicts(input)
-    return predicted_message
+    predicted_message = get_answer(input)
+    return jsonify(predicted_message)
+
+# Initialize Flask server (file predictionv3.py)
+@app.route("/chat", methods=["GET", "POST"])
+def chat():
+    input = request.form['input']
+
+    chatbotReply = get_answer(input)
+    return jsonify(chatbotReply)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
-    # app.run()
+    # app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(port=8080, debug=True)
